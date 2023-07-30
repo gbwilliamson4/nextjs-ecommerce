cd74ae1927ad2d8c98041be535ade1f65047cc36
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2023-07-30 06:53:09 -0600

    added functionality to add-product

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: 'images.unsplash.com' }],
+  },
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  experimental: {
+    serverActions: true,
+  },
+  nextConfig,
+};
---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,54 @@
+import FormSubmitButton from '@/components/FormSubmitButton';
+import { prisma } from '@/lib/prisma';
+import { redirect } from 'next/navigation';
+
+export const metadata = {
+  title: 'Add Product - Flowmazon',
+};
+
+async function addProduct(formData: FormData) {
+  'use server';
+
+  const name = formData.get('name')?.toString();
+  const description = formData.get('description')?.toString();
+  const imageUrl = formData.get('imageUrl')?.toString();
+  const price = Number(formData.get('price') || 0);
+
+  //   throw Error('Testing.');
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error('Missing fields required');
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect('/');
+}
+
+export default function AddProductPage() {
+  return (
+    <div>
+      <h1 className="text-lg mb-3 font-bold">Add product</h1>
+      <form action={addProduct}>
+        <input required name="name" placeholder="Name" className="mb-3 w-full input input-bordered" />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full"
+        />
+        <input
+          required
+          name="imageUrl"
+          placeholder="Image URL"
+          type="url"
+          className="mb-3 w-full input input-bordered"
+        />
+        <input required name="price" placeholder="Price" type="number" className="mb-3 w-full input input-bordered" />
+        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
+      </form>
+    </div>
+  );
+}
