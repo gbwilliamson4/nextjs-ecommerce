a1e3fa441c1314acb196f05d012ac016cdd4d7f5
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2023-07-31 20:59:07 -0600

    Added shopping cart card and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Navbar from './Navbar/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
       </body>
     </html>
