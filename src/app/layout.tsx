3b44d1d1d48bafb55b857fb90ed77d18db71068f
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2023-08-18 06:47:23 -0600

    Added next-auth functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navbar from './Navbar/Navbar';
+import Footer from './Footer';
+import SessionProvider from './SessionProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,8 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+        <SessionProvider>
+          <Navbar />
+          <main className="p-4 max-w-7xl m-auto min-w-[300px]">{children}</main>
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
